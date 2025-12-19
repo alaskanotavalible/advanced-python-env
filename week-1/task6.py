43bfc24e399c17e3d59ce208016874cd2124fc75
alaskanotavalible <aliaskarmnv2@gmail.com>
2025-12-19 16:50:25 +0500

    week 1 tasks

---
+++ week-1/task6.py
@@ -0,0 +1,12 @@
+a = float(input("First number: "))
+operation = input("Operation: ")
+b = float(input("Second number: "))
+
+if operation == "+":
+    print(a + b)
+elif operation == "-":
+    print(a - b)
+elif operation == "*":
+    print(a * b)
+elif operation == "/":
+    print(a / b)
